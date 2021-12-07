164949ad5cf92ff83d71a0343a0bbb4d25a09e08
Reiya <reiya0104@ezweb.ne.jp>
2021-12-07 19:01:38 +0900

    Adapt Alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,22 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   // 本番時はgithubリポジトリをルートパスにする
   base: (process.env.NODE_ENV === 'production')
-    ? '/vite-vue-router-tailwind-test/' : './',
+    ? '/vue-google-analytics-test/' : './',
   build: {
     outDir: 'docs'
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      '@c': resolve(__dirname, 'src/components'),
+      '@v': resolve(__dirname, 'src/views'),
+      // '@u': resolve(__dirname, 'src/utils'),
+    }
   },
   plugins: [vue()]
 })
